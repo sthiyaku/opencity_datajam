958c5e9158a07807f3fe6f7cca71abc67401b1f8
sthiyaku <35148061+sthiyaku@users.noreply.github.com>
2023-04-11 23:28:34 +0530

    Simplied code

--- src/classification_works.py
+++ src/classification_works.py
@@ -1,112 +1,66 @@
 import pandas as pd
 import numpy as np
-pd.options.mode.chained_assignment = None  # default='warn'
 
+# Read input CSV file
 in_file = '/home/thiyaku/Projects/personal/datajam/csv/merged_ward_data.csv'
-
 df = pd.read_csv(in_file)
+
+# Disable chained assignment warning
+pd.options.mode.chained_assignment = None
+
+
+# Function to assign categories to rows based on keywords
+def assign_category(df, col, keywords, category):
+    for keyword in keywords:
+        mask = df[col].str.contains(keyword, case=False)
+        df['Category'].loc[mask] = category
+    return df
+
+
+# Define keyword-category mappings
+keyword_categories = {
+    'road_keywords': ['pot hole', 'pothole', 'road', 'asphalt', 'fly over',
+                      'flyover'],
+    'footpath_keywords': ['footpath', 'foot path'],
+    'drainage_keywords': ['drain', 'drian', 'ugd', 'rcc dra', 'culvert'],
+    'electricity_keywords': ['electric', 'light', 'illumination'],
+    'women_keywords': ['stri shakthi'],
+    'dw_keywords': ['drinking water', 'drinking', 'supply of water',
+                    'water supply', 'r o'],
+    'gw_keywords': ['in filtration well', 'infiltration well', 'borewell',
+                    'borwell', 'water har'],
+    'toilet_keywords': ['toilet'],
+    'c_b_keywords': ['crematorium', 'burial ground'],
+    'health_keywords': ['hospital', 'moh building'],
+    'school_keywords': ['school', 'anganawadi', 'anganavadi', 'anganwadi'],
+    'poll_station_keywords': ['polling station'],
+    'skill_devp_keywords': ['training', 'skill dev', 'reading ro'],
+    'park_keywords': ['park'],
+    'samudhaya_keywords': ['samudhaya', 'samudaya'],
+    'library_keywords': ['library', 'libraury'],
+    'gym_yoga_keywords': ['gym', 'yoga'],
+    'sports_keywords': ['stadium', 'badminton', 'play eq', 'play gr'],
+    'indira_canteen_keywords': ['indira canteen'],
+    'HoW_keywords': ['temple', 'ganesha', 'prayer'],
+    'name_board_keywords': ['name board', 'name bord'],
+    'emergency_keywords': ['emergency work']
+}
 df['Category'] = ''
+# Assign categories to rows based on keywords
+for key, value in keyword_categories.items():
+    df = assign_category(df, 'Name_of_Work', value, key.split('_')[0].title())
 
-col = 'Name_of_Work'
-df[col] = df[col].str.lower()
-df.Name_of_Work = df.Name_of_Work.replace(r'\s+', ' ', regex=True)
+# Get rows with empty 'Category' column
+empty_category_rows = df[df['Category'] == '']
 
-road_keywords = ['pot hole', 'pothole', 'road', 'asphalt', 'fly over',
-                 'flyover']
-for keyword in road_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Road'
+# Sort empty category rows by 'Name_of_Work' column
+empty_category_rows = empty_category_rows.sort_values('Name_of_Work')
 
-drainage_keywords = ['drain', 'drian', 'ugd', 'rcc dra', 'culvert']
-for keyword in drainage_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Drainage'
+# Assign 'Category' as 'Unknown' for remaining rows with empty 'Category' column
+df['Category'].fillna('Unknown', inplace=True)
 
-electricity_keywords = ['electric', 'light', 'illumination']
-for keyword in electricity_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Electric work'
-    
-women_keywords = ['stri shakthi']
-for keyword in women_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Women empowerment'
+# Reset index of empty category rows
+empty_category_rows.reset_index(drop=True, inplace=True)
 
-dw_keywords = ['drinking water', 'drinking', 'supply of water', 'water supply']
-for keyword in dw_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Drinking water'
-
-gw_keywords = ["in filtration well", "infiltration well", "borewell"]
-for keyword in gw_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Ground water'
-
-
-mask = np.column_stack([df[col].str.contains("toilet")])
-df['Category'].loc[mask] = 'Toilets'
-
-c_b_keywords = ['crematorium', 'burial ground']
-for keyword in c_b_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Crematorium/burial ground'
-
-health_keywords = ['hospital', 'moh building']
-for keyword in health_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Healthcare'
-
-school_keywords = ['school', 'anganawadi', 'anganavadi']
-for keyword in school_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'School'
-
-mask = np.column_stack([df[col].str.contains("polling station")])
-df['Category'].loc[mask] = 'Poll station'
-
-skill_devp_keywords = ['training', 'skill dev', 'reading ro']
-for keyword in skill_devp_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Skill development'
-
-mask = np.column_stack([df[col].str.contains("park")])
-df['Category'].loc[mask] = 'Parks'
-samudhaya_keywords = ['samudhaya', 'samudaya']
-for keyword in samudhaya_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Samudhaya bhavana'
-
-library_keywords = ['library', 'libraury']
-for keyword in library_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Library'
-
-gym_yoga_keywords = ['gym', 'yoga']
-for keyword in gym_yoga_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Gym/Yoga'
-
-sports_keywords = ['stadium', 'badminton', 'play eq']
-for keyword in sports_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Sports'
-
-indira_canteen_keywords = ['indira canteen']
-for keyword in indira_canteen_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Indira canteen'
-
-HoW_keywords = ['temple', 'ganesha']
-for keyword in HoW_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'House of worship'
-
-name_board_keywords = ['name_board', 'name_bord']
-for keyword in name_board_keywords:
-    mask = np.column_stack([df[col].str.contains(keyword)])
-    df['Category'].loc[mask] = 'Name board'
-
-empty_cols = df['Category'] == ''
-foo = df.loc[empty_cols].reset_index()
-print(foo['Name_of_Work'][1])
-print(np.nansum(empty_cols))
+# Display final DataFrame
+print(df)
