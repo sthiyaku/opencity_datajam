f3e92fbe2f860a5e9318b9187038165a5864cabf
sthiyaku <35148061+sthiyaku@users.noreply.github.com>
2023-04-12 00:07:11 +0530

    Added few new categories

--- src/classification_works.py
+++ src/classification_works.py
@@ -4,7 +4,6 @@ import numpy as np
 # Read input CSV file
 in_file = '/home/thiyaku/Projects/personal/datajam/csv/merged_ward_data.csv'
 df = pd.read_csv(in_file)
-
 # Disable chained assignment warning
 pd.options.mode.chained_assignment = None
 
@@ -19,32 +18,46 @@ def assign_category(df, col, keywords, category):
 
 # Define keyword-category mappings
 keyword_categories = {
-    'road_keywords': ['pot hole', 'pothole', 'road', 'asphalt', 'fly over',
-                      'flyover'],
+    'road_keywords': ['pot hole', 'pothole', 'patholes', 'road', 'asphalt',
+                      'fly over', 'flyover', 'cement concrete', 'cc patch',
+                      'bridge'],
     'footpath_keywords': ['footpath', 'foot path'],
-    'drainage_keywords': ['drain', 'drian', 'ugd', 'rcc dra', 'culvert'],
+    'drainage_keywords': ['drain', 'drian', ' ugd ', 'rcc dra', 'culvert',
+                          'sw line', 'silt', ' swd '],
     'electricity_keywords': ['electric', 'light', 'illumination'],
     'women_keywords': ['stri shakthi'],
-    'dw_keywords': ['drinking water', 'drinking', 'supply of water',
-                    'water supply', 'r o'],
-    'gw_keywords': ['in filtration well', 'infiltration well', 'borewell',
-                    'borwell', 'water har'],
+    'drinking water_keywords': ['drinking water', 'drinking', 'r o', 'ro plant',
+                                'supply of water', 'supplying of water',
+                                'water supply'],
+    'ground water_keywords': ['in filtration well', 'infiltration well',
+                              'borewell', 'borwell', 'water har'],
     'toilet_keywords': ['toilet'],
     'c_b_keywords': ['crematorium', 'burial ground'],
-    'health_keywords': ['hospital', 'moh building'],
-    'school_keywords': ['school', 'anganawadi', 'anganavadi', 'anganwadi'],
+    'health care_keywords': ['hospital', 'moh ', 'phc ', 'sanitary', 'doctor',
+                             'primary hea', 'covid'],
+    'school or college_keywords': ['school', 'anganawadi', 'anganavadi',
+                                   'anganwadi', 'pu college', 'college'],
     'poll_station_keywords': ['polling station'],
-    'skill_devp_keywords': ['training', 'skill dev', 'reading ro'],
+    'skill_development_keywords': ['training', 'skill dev', 'reading ro',
+                                   'laptop'],
     'park_keywords': ['park'],
     'samudhaya_keywords': ['samudhaya', 'samudaya'],
     'library_keywords': ['library', 'libraury'],
     'gym_yoga_keywords': ['gym', 'yoga'],
-    'sports_keywords': ['stadium', 'badminton', 'play eq', 'play gr'],
-    'indira_canteen_keywords': ['indira canteen'],
-    'HoW_keywords': ['temple', 'ganesha', 'prayer'],
+    'sports_keywords': ['stadium', 'badminton', 'play eq', 'play gr', 'playgr'
+                        'basket ball', 'kabbadi', 'volley ball', 'sports'],
+    'indira canteen_keywords': ['indira canteen', 'indrira canteen',
+                                'canteen'],
+    'House of Worship_keywords': ['temple', 'ganesha', 'prayer'],
     'name_board_keywords': ['name board', 'name bord'],
-    'emergency_keywords': ['emergency work']
+    'emergency_keywords': ['emergency work'],
+    'EWS_keywords': ['ews house'],
+    'waste management_keywords': ['sweep smart'],
+    'maintenance_keywords': ['annual maint', 'maintenance']
 }
+
+df.Name_of_Work = df.Name_of_Work.replace(r'\s+', ' ', regex=True)
+
 df['Category'] = ''
 # Assign categories to rows based on keywords
 for key, value in keyword_categories.items():
@@ -63,4 +76,4 @@ df['Category'].fillna('Unknown', inplace=True)
 empty_category_rows.reset_index(drop=True, inplace=True)
 
 # Display final DataFrame
-print(df)
+print(empty_category_rows)
